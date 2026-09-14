d6ccee614efb6d3dba636b1a45884e6dd04943c1
289rishi <rishi.tiwari289@gmail.com>
2026-09-15 00:01:09 +0530

    Update Test1Application.java

--- src/main/java/com/test1/Test1Application.java
+++ src/main/java/com/test1/Test1Application.java
@@ -9,6 +9,7 @@ public class Test1Application {
 	public static void main(String[] args) {
 		int x=10;
 		int y=25;
+		boolean z= false;
 		SpringApplication.run(Test1Application.class, args);
 	}
 
